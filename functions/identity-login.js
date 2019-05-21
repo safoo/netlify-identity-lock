17266eda77d85b642cdad1f5af91853d9ec88eef
Sarfaraz Rydhan <sarfaraz@Sarfarazs-MacBook-Pro.local>
2019-05-21 00:34:13 -0700

    add functions

---
+++ functions/identity-login.js
@@ -0,0 +1,26 @@
+cmsEmailWhitelist = [`netlify.com`,]
+
+export function handler({ body }, _, callback){
+	const { user } = JSON.parse(body)
+	const { email } = user
+	console.log(`${email} signing up`)
+	const domain = email.split(`@`)[1]
+	let res = ``
+	let statusCode = 400
+	if(cmsEmailWhitelist.indexOf(domain) !== -1){
+		console.log(`Whitelisting`)
+		statusCode = 200
+		res = JSON.stringify({
+			app_metadata: {
+				roles: [`admin`],
+			},
+		})
+	}
+	else{
+		console.log(`Blocking`)
+	}
+	callback(null, {
+		statusCode,
+		body: res,
+	})
+}
