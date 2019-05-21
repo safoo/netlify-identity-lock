f376f3269c286fce33c96daddd4352bec33140a5
Sarfaraz Rydhan <sarfaraz@Sarfarazs-MacBook-Pro.local>
2019-05-21 05:27:57 -0700

    function

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,36 +1,3 @@
-// export function handler({ body }, _, callback){
-// 	const { user } = JSON.parse(body)
-// 	const { email } = user
-// 	console.log(`${email} signing up`)
-// 	const domain = email.split(`@`)[1]
-// 	let res = ``
-//     let statusCode = 400
-    
-//     cmsEmailWhitelist = [`netlify.com`,]
-
-// 	if(cmsEmailWhitelist.indexOf(domain) !== -1){
-// 		console.log(`Whitelisting`)
-// 		statusCode = 200
-// 		res = JSON.stringify({
-// 			app_metadata: {
-// 				roles: [`admin`],
-// 			},
-// 		})
-// 	}
-// 	else{
-// 		console.log(`Blocking`)
-// 	}
-// 	callback(null, {
-// 		statusCode,
-// 		body: res,
-// 	})
-// }
-
-// export function(event, context, callback) {
-//     const {identity, user} = context.clientContext;
-//     // Do stuff...
-//   }
-
 exports.handler = function(event, context, callback) {
     // your server-side functionality
     const { user } = JSON.parse(event.body)
@@ -50,10 +17,10 @@ exports.handler = function(event, context, callback) {
 				roles: [`admin`],
 			},
 		})
-	}
-	else{
+	} else {
 		console.log(`Blocking`)
-	}
+    }
+    
 	callback(null, {
 		statusCode,
 		body: res,
