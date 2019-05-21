f88ab61139a5d6bb79da64bfe780a4ec66e37bfb
Sarfaraz Rydhan <sarfaraz@Sarfarazs-MacBook-Pro.local>
2019-05-21 06:37:37 -0700

    functions

---
+++ functions/identity-check-login.js
@@ -0,0 +1,28 @@
+exports.handler = function(event, context, callback) {
+    // your server-side functionality
+    const { user } = JSON.parse(event.body)
+	const { email } = user
+	console.log(`${email} signing up`)
+	const domain = email.split(`@`)[1]
+	let res = ``
+    let statusCode = 400
+    
+    cmsEmailWhitelist = [`netlify.com`,]
+
+	if(cmsEmailWhitelist.indexOf(domain) !== -1){
+		console.log(`Whitelisting`)
+		statusCode = 200
+		res = JSON.stringify({
+			app_metadata: {
+				roles: [`loggedinuser`],
+			},
+		})
+	} else {
+		console.log(`Blocking`)
+    }
+    
+	callback(null, {
+		statusCode,
+		body: res,
+	})
+}
