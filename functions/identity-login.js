47deb02ddde4c7b18ff9a50ba7a255e90e08e2c7
Sarfaraz Rydhan <sarfaraz@Sarfarazs-MacBook-Pro.local>
2019-05-21 01:19:56 -0700

    function

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -1,27 +1,38 @@
-export function handler({ body }, _, callback){
-	const { user } = JSON.parse(body)
-	const { email } = user
-	console.log(`${email} signing up`)
-	const domain = email.split(`@`)[1]
-	let res = ``
-    let statusCode = 400
+// export function handler({ body }, _, callback){
+// 	const { user } = JSON.parse(body)
+// 	const { email } = user
+// 	console.log(`${email} signing up`)
+// 	const domain = email.split(`@`)[1]
+// 	let res = ``
+//     let statusCode = 400
     
-    cmsEmailWhitelist = [`netlify.com`,]
+//     cmsEmailWhitelist = [`netlify.com`,]
 
-	if(cmsEmailWhitelist.indexOf(domain) !== -1){
-		console.log(`Whitelisting`)
-		statusCode = 200
-		res = JSON.stringify({
-			app_metadata: {
-				roles: [`admin`],
-			},
-		})
-	}
-	else{
-		console.log(`Blocking`)
-	}
-	callback(null, {
-		statusCode,
-		body: res,
-	})
+// 	if(cmsEmailWhitelist.indexOf(domain) !== -1){
+// 		console.log(`Whitelisting`)
+// 		statusCode = 200
+// 		res = JSON.stringify({
+// 			app_metadata: {
+// 				roles: [`admin`],
+// 			},
+// 		})
+// 	}
+// 	else{
+// 		console.log(`Blocking`)
+// 	}
+// 	callback(null, {
+// 		statusCode,
+// 		body: res,
+// 	})
+// }
+
+// export function(event, context, callback) {
+//     const {identity, user} = context.clientContext;
+//     // Do stuff...
+//   }
+
+exports.handler = function(event, context, callback) {
+    // your server-side functionality
+    console.log('event',event);
+    console.log('context',context);
 }
