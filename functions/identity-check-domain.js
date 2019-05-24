13fa16d2a79f5659ac853ef36985cb18251ed554
Sarfaraz Rydhan <sarfaraz@Sarfarazs-MacBook-Pro.local>
2019-05-23 21:24:38 -0700

    cleanup and remove setting cookie

--- functions/identity-check-domain.js
+++ functions/identity-check-domain.js
@@ -1,29 +1,34 @@
 exports.handler = function(event, context, callback) {
+    /*
+     * Credit https://github.com/kennedyrose
+     * Source: https://github.com/escaladesports/react-netlify-form/blob/master/src/functions/identity-signup.js
+     */
+
     // your server-side functionality
     const { user } = JSON.parse(event.body)
     const { email } = user
 
-	console.log(`${email} signing up`)
-	const domain = email.split(`@`)[1]
-	let res = ``
+    console.log(`${email} signing up`)
+    const domain = email.split(`@`)[1]
+    let res = ``
     let statusCode = 400
     
     domainWhitelist = [`netlify.com`,]
 
-	if(domainWhitelist.indexOf(domain) !== -1){
-		console.log(`Whitelisting`)
-		statusCode = 200
-		res = JSON.stringify({
-			app_metadata: {
-				roles: [`loggedinuser`],
-			},
-		})
-	} else {
-		console.log(`Blocking`)
+    if(domainWhitelist.indexOf(domain) !== -1){
+        console.log(`Whitelisting`)
+        statusCode = 200
+        res = JSON.stringify({
+            app_metadata: {
+                roles: [`loggedinuser`],
+            },
+        })
+    } else {
+        console.log(`Blocking`)
     }
     
-	callback(null, {
-		statusCode,
-		body: res,
-	})
+    callback(null, {
+        statusCode,
+        body: res,
+    })
 }
